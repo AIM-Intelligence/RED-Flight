fe91ddfb8dabb5a7b7e112c4c1bcd9702ac029dd
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-05-27 22:38:39 +0900

    feat: :tada: set eslint prettiierrc

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  eslint: {
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+  },
+  images: {
+    domains: ["images.pexels.com", "lh3.googleusercontent.com", "avatars.githubusercontent.com"],
+  },
+};
 
 export default nextConfig;
