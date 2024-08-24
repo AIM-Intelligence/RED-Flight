642f3b4c4c6907002b0c80886017461f9dbc3e47
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-08-24 18:28:58 +0900

    feat: :sparkles: fix auth router error, update user, prompt data when clear game

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -3,10 +3,17 @@
 import { Suspense } from "react";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/Button";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
   return (
     <main className="flex h-screen items-center justify-center bg-black">
+      <Button className="absolute top-10 z-10" onClick={() => router.push("/")}>
+        Go home
+      </Button>
       <Image
         src="/background/zaion_city.jpg"
         className="absolute inset-0 h-full w-full"
