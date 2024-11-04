be5e0644b55bc0fbaa8bdd62ccf32aa930e261b6
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-11-05 02:18:20 +0900

    feat: :zap: npm update & add near network

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -1,14 +1,15 @@
 import { defineChain } from "thirdweb";
 
 //const zetaT = defineChain(7001);
-const mantaT = defineChain(3441006);
+//const mantaT = defineChain(3441006);
 const sepolia = defineChain(11155111);
 const ether = defineChain(1);
+const near = defineChain(397);
 //const kaiaT = defineChain(1001);
 //const zkSyncT = defineChain(300);
 //const coreT = defineChain(1115);
 //const theta = defineChain(365);
 
-export const chainList = [mantaT, sepolia, ether];
+export const chainList = [near, ether, sepolia];
 
 export const chain = defineChain(3441006);
