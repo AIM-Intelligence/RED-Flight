f975819bfe0b7609c12b4212a1483017f204006f
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-11-05 17:02:55 +0900

    feat: add aurora network

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -4,12 +4,12 @@ import { defineChain } from "thirdweb";
 //const mantaT = defineChain(3441006);
 const sepolia = defineChain(11155111);
 const ether = defineChain(1);
-const near = defineChain(397);
+const aurora = defineChain(1313161554);
 //const kaiaT = defineChain(1001);
 //const zkSyncT = defineChain(300);
 //const coreT = defineChain(1115);
 //const theta = defineChain(365);
 
-export const chainList = [near, ether, sepolia];
+export const chainList = [aurora, ether, sepolia];
 
 export const chain = defineChain(3441006);
