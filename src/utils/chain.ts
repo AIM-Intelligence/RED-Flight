d48be7d44e608a3617599e909bd516a77cd5d656
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-11-05 19:05:04 +0900

    feat: :zap: add aurora network nft drop function

--- src/utils/chain.ts
+++ src/utils/chain.ts
@@ -3,13 +3,14 @@ import { defineChain } from "thirdweb";
 //const zetaT = defineChain(7001);
 //const mantaT = defineChain(3441006);
 const sepolia = defineChain(11155111);
-const ether = defineChain(1);
-const aurora = defineChain(1313161554);
+// const ether = defineChain(1);
+// const aurora = defineChain(1313161554);
+const auroraT = defineChain(1313161555);
 //const kaiaT = defineChain(1001);
 //const zkSyncT = defineChain(300);
 //const coreT = defineChain(1115);
 //const theta = defineChain(365);
 
-export const chainList = [aurora, ether, sepolia];
+export const chainList = [auroraT, sepolia];
 
-export const chain = defineChain(3441006);
+export const chain = defineChain(1313161555);
--- src/utils/explore.ts
+++ src/utils/explore.ts
@@ -4,6 +4,8 @@ export const getExplorerApiUrl = (chainId: number) => {
       return "https://eth-sepolia.blockscout.com/api/v2/transactions/";
     case 3441006:
       return "https://manta-sepolia.explorer.caldera.xyz/api/v2/transactions/";
+    case 1313161555:
+      return "https://explorer.testnet.aurora.dev/api/v2/transactions/";
     default:
       throw new Error("Unsupported chainId");
   }
