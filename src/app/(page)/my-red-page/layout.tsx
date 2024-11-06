2f25ef0b78d751bb7633eb6871866c42ce356dd3
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-11-06 12:13:58 +0900

    feat: set eslint

--- src/app/(page)/my-red-page/layout.tsx
+++ src/app/(page)/my-red-page/layout.tsx
@@ -1,28 +1,22 @@
-import Image from "next/image";
-import { redirect } from "next/navigation";
+import { Metadata } from 'next';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
 
-import { Metadata } from "next";
-
-import { isLoggedIn } from "@/server/auth/auth";
+import { isLoggedIn } from '@/server/auth/auth';
 
 export const metadata: Metadata = {
-  title: "Red Flight My Page",
-  description: "AI Jailbreaking NFT Game",
+  title: 'Red Flight My Page',
+  description: 'AI Jailbreaking NFT Game',
 };
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   if (!(await isLoggedIn())) {
-    redirect("/");
+    redirect('/');
   }
 
   return (
     <main className="relative min-h-screen bg-black font-sora text-white">
-      <Image
-        src="/background/05.png"
-        alt="Background"
-        fill
-        style={{ objectFit: "cover" }}
-      />
+      <Image src="/background/05.png" alt="Background" fill style={{ objectFit: 'cover' }} />
       <div className="relative z-10 pb-20">{children}</div>
     </main>
   );
