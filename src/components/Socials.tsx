c557c3e66ec254f29c039aea342844c04a7237e1
Youjun <tndhworl1998@gmail.com>
2025-03-22 22:58:44 +0900

    feat: :sparkles: add seo and link

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,35 +1,30 @@
 import Link from 'next/link';
-import {
-  RiDiscordLine,
-  RiInstagramLine,
-  RiTwitterXLine,
-  RiYoutubeLine,
-} from 'react-icons/ri';
+import { RiInstagramLine, RiTwitterXLine, RiYoutubeLine } from 'react-icons/ri';
 
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5">
       <Link
-        href="https://www.youtube.com/@zAIon-c3d"
+        href="https://www.youtube.com/@aim-intelligence"
         className="transition-all duration-300"
       >
         <RiYoutubeLine className="h-8 w-8" />
       </Link>
       <Link
-        href="https://x.com/redflightAI"
+        href="https://x.com/AIM408191939218"
         className="transition-all duration-300"
       >
         <RiTwitterXLine className="h-8 w-8" />
       </Link>
-      <Link
+      {/* <Link
         href="https://discord.gg/HyuhgvGBu9"
         target="_blank"
         className="transition-all duration-300"
       >
         <RiDiscordLine className="h-8 w-8" />
-      </Link>
+      </Link> */}
       <Link
-        href="https://www.instagram.com/redflightai"
+        href="https://www.instagram.com/aim_intelligence/"
         target="_blank"
         className="transition-all duration-300"
       >
